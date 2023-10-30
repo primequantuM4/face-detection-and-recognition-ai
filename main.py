b4297792fe985401ea6b08aa1aad5da39091bf92
Nathnael Dereje <nathandere1357@gmail.com>
2023-10-30 16:36:42 +0300

    Write main entry point

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+from matplotlib import pyplot as plt
 from util.file_exension_editor import add_file_extension, remove_file_extension
 from util.image_cropper import crop_images
+
+
+def visualize_cropped_images(cropped_images):
+    """ This function visualizes the cropped images. """
+
+    num_images = len(cropped_images)
+    rows = 10
+    cols = (num_images + 1) // rows
+
+    fig, axes = plt.subplots(rows, cols)
+
+    for i, ax in enumerate(axes.flat):
+        ax.axis("off")
+        if i < num_images:
+            ax.imshow(cropped_images[i], cmap="gray")
+
+    plt.show()
+
 
 def main():
     """ This function is the main function of the module. """
@@ -7,8 +26,7 @@ def main():
     dataset_dir = "dataset/data"
     remove_file_extension(dataset_dir)
     add_file_extension(dataset_dir, ".png")
-    print(crop_images(dataset_dir))
-    
+    visualize_cropped_images(crop_images(dataset_dir))
 
 
 if __name__ == "__main__":
